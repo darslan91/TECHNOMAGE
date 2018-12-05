13cd1615a413a709d108bd6735f750bd5ff19dd0
Rawzbeef <vincent.gaspar@hotmail.fr>
2018-12-05 15:17:32 +0100

    JFrame

--- src/fr/technomage/controleur/Controleur.java
+++ src/fr/technomage/controleur/Controleur.java
@@ -1,8 +1,11 @@
 package fr.technomage.controleur;
 
+import fr.technomage.vue.Vue;
+
 public class Controleur {
 
 	public static void main(String[] args) {
-		
+		Vue vue = new Vue();
+		vue.setVisible(true);
 	}
 }
--- src/fr/technomage/vue/Vue.java
+++ src/fr/technomage/vue/Vue.java
@@ -1,5 +1,14 @@
 package fr.technomage.vue;
 
-public class Vue {
+import javax.swing.JFrame;
 
+@SuppressWarnings("serial")
+public class Vue extends JFrame{
+	public Vue() {
+		this.setTitle("Technomage");
+		this.setSize(1600, 900);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+	}
 }
